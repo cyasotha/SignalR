de4790e75498a3874b8e689f015bec8de171a6c2
Yasotha Chandrakumaran <ychandrakumaran@erwin.com>
2017-03-30 15:02:25 +0100

    SignalR

---
+++ SignalR/Scripts/app/SignalR.js
@@ -0,0 +1,25 @@
+﻿(function($) {
+    var chat = $.connection.chatHub;
+    chat.client.addNewMessageToPage = function (name, message) {
+        // Add the message to the page.
+        $('#discussion').append('<li><strong>' + htmlEncode(name)
+            + '</strong>: ' + htmlEncode(message) + '</li>');
+    };
+    // Get the user name and store it to prepend to messages.
+    $('#displayname').val(prompt('Enter your name:', ''));
+    // Set initial focus to message input box.
+    $('#message').focus();
+    $.connection.hub.start().done(function() {
+        $('#sendmessage').click(function () {
+            // Call the Send method on the hub.
+            chat.server.send($('#displayname').val(), $('#message').val());
+            // Clear text box and reset focus for next comment.
+            $('#message').val('').focus();
+        });
+    });
+    // This optional function html-encodes messages for display in the page.
+    function htmlEncode(value) {
+        var encodedValue = $('<div />').text(value).html();
+        return encodedValue;
+    }
+})(jQuery)
